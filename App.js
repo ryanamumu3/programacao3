040402d68617ddd2370f5690e8edbb157d360471
emilyviih07 <evna@discente.ifpe.edu.br>
2025-03-27 08:36:20 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ruan Nasciento Monteiro da Silva</Text>
+      <Text>Emily vitoria do nascimento arruda</Text>
+      <text>Gustavo Da Silva Araújo</text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
